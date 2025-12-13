5652868c469ef3b9cab101efd72b6765c3be7621
FAZE2000iQ <collinlovespiano@gmail.com>
2025-12-13 15:10:56 -0800

    Add code verification function

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+function checkCode() {
+  const correctCode = "1234"; // CHANGE THIS
+  const userCode = document.getElementById("codeInput").value;
 
+  if (userCode === correctCode) {
+    window.location.href = "secret.html";
+  } else {
+    alert("Wrong code!");
+  }
+}
